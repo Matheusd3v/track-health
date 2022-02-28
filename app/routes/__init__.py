2aa8ae26f47a05e28f479864fd2e9a25d500e098
Guilhermino Lucas <guilherminolucasc@gmail.com>
2022-02-28 15:10:56 -0300

    Feat: doing the address routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from app.routes.user_route import bp_user
+from app.routes.address_route import bp_address
 
 def init_app(app: Flask):
     app.register_blueprint(bp_user)
+    app.register_blueprint(bp_address)
