25fbfc90f30ab4c001a63e72275489fcd9b99ead
Eric Esteves Martins <85449830+ericmartins0203@users.noreply.github.com>
2022-02-28 11:38:25 -0300

    Update exam_model.py

--- app/models/exam_model.py
+++ app/models/exam_model.py
@@ -7,5 +7,8 @@ from sqlalchemy.dialects.postgresql import UUID
 
 @dataclass
 class Exam(db.Model):
+    
+    __tablename__ = "exams"
+    
     id: str = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     name:str = Column(String, nullable = False, unique = True)
