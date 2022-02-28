d22f1f63d5d0d9be79a1ecb9468a03de847f485d
Gustavo <go8895806@gmail.com>
2022-02-28 11:09:57 -0300

    feat: created exam table

---
+++ app/models/exam_model.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from app.configs.database import db
+from sqlalchemy import Column, String
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
+
+
+@dataclass
+class Exam(db.Model):
+    id: str = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    name:str = Column(String, nullable = False, unique = True)
