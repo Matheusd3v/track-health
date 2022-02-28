75f32cb80b3ba4a70902c1e48bd121c5068d1f26
Eric Esteves Martins <85449830+ericmartins0203@users.noreply.github.com>
2022-02-28 11:40:53 -0300

    Update exam_details_model.py

--- app/models/exam_details_model.py
+++ app/models/exam_details_model.py
@@ -12,7 +12,8 @@ class ExamDetails(db.Model):
     __tablename__ = "exam_details"
 
     id: str = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    user_id: str = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable = False)
     date:str = Column(Date)
     upload_img:str = Column(String)
     description:str = Column(String) 
+        
+    user_id: str = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable = False)
