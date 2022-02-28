19a0a39989bf750e7096f0acc256b66120375385
Gustavo <go8895806@gmail.com>
2022-02-28 16:43:49 -0300

    feat: created the anamnesis route update and create

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from app.routes.user_route import bp_user
+from app.routes.anamnesis_route import bp as bp_anamnesis
+
 
 def init_app(app: Flask):
     app.register_blueprint(bp_user)
+    app.register_blueprint(bp_anamnesis)
