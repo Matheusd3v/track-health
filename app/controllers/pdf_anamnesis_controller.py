ca67923d356d238da8f4381f49a57b9ac3f366f2
Gustavo <go8895806@gmail.com>
2022-03-05 11:19:25 -0300

    feat: done part of the template

--- app/controllers/pdf_anamnesis_controller.py
+++ app/controllers/pdf_anamnesis_controller.py
@@ -1,15 +1,19 @@
 from flask import jsonify, render_template, make_response
 import pdfkit
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models.user_model import User
 
 from app.services.pdf_anamnesis_services import get_anamnesis
 
 @jwt_required()
 def create_pdf():
     user = get_jwt_identity()
+    user = User.query.filter_by(id = user["id"]).first()
+    user = user.asdict()
+
     anamnesis = get_anamnesis(user["id"])
 
-    rendered = render_template("anamnesis.html", anamnesis=anamnesis, name=user["name"], )
+    rendered = render_template("anamnesis.html", anamnesis=anamnesis, name=user["name"], user=user)
     pdf = pdfkit.from_string(rendered, False)
 
     response = make_response(pdf)
